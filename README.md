6fab0868fdc49f4214338ea8d61898b1344802f6
SriHarsha Godavarthi <sriharsha.godavarthi232@gmail.com>
2025-02-06 13:23:57 -0500

    reduxShoppingCart

---
+++ README.md
@@ -0,0 +1,39 @@
+# Redux Shopping Cart
+
+### [Demo Link]() 🔗
+
+**Shopping Cart Project** built with **React** + **Redux-Toolkit**.
+
+<br/>
+
+## Features:
+
+- #### Add to Cart
+- #### Remove from Cart
+- #### Increment / Decrement the Quantity
+
+<br/>
+
+## Technologies used:
+
+- #### **React JS**
+- #### **Redux Toolkit**
+- #### **SASS**
+
+<br/>
+
+## Author:
+
+- **Sri Harsha G** - **[Portfolio Website](https://harsha-godavarthi-portfolio.netlify.app/)**, **[Linkedin](www.linkedin.com/in/sri-harsha-godavarthi-2895a0185)**
+
+<br/>
+
+## Available Scripts:
+
+#### `npm start`
+
+#### `npm test`
+
+#### `npm run build`
+
+#### `npm run eject`
---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://github.com/SriHarsha-Godavarthi/redux-shopping-cart"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        github.com/SriHarsha-Godavarthi/redux-shopping-cart
+                    </a>
+
+                    <p>
+                        Built by | &nbsp;
+                        <a
+                            href=""
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Sri Harsha
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
